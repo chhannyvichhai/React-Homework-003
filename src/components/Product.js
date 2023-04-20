2937b7abb3571305ec07e2b74ed01e4c4efef733
Chhanny-Vichhai <vichhai212803@gmail.com>
2023-04-20 08:44:02 +0700

    Update code

--- src/components/Product.js
+++ src/components/Product.js
@@ -25,42 +25,20 @@ const ProductCollection = () => {
 			<WaitingData />
 		</>
 	) : (
-		products.map((product) => (
-			<div className="container d-flex justify-content-center">
+	<div className="container">
 				<div className="row mt-5">
-					<div className="col-12 col-sm-12 col-md-4 col-lg-4">
-						<div class="card" style={{width: 300}}>
-							<img src={product.images[0]} class="card-img-top" alt="..." />
-							<div class="card-body">
-								<p class="card-text">
-									{product.title}
-								</p>
-							</div>
-						</div>
-					</div>
-					<div className="col-12 col-sm-12 col-md-4 col-lg-4">
-						<div class="card" style={{width: 300}}>
-							<img src={product.images[1]} class="card-img-top" alt="..." />
-							<div class="card-body">
-								<p class="card-text">
-									{product.title}
-								</p>
-							</div>
-						</div>
-					</div>
-					<div className="col-12 col-sm-12 col-md-4 col-lg-4">
-						<div class="card" style={{width: 300}}>
-							<img src={product.images[2]} class="card-img-top" alt="..." />
-							<div class="card-body">
-								<p class="card-text">
-									{product.title}
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		))
+	{products.map((product) =>(
+		<div className="col-12 col-sm-12 col-md-4 col-lg-3">
+			<div class="card" style={{width: 300}}>
+		<img src={product.images} class="card-img-top" alt="..."/>
+		<div class="card-body">
+		  <p class="card-text">{product.title}</p>
+		</div>
+	  </div>
+		</div>
+	))}
+		</div>
+	</div>
 	);
 };
 
@@ -69,6 +47,24 @@ function WaitingData() {
 		<>
 			<div className="container">
 				<div className="row">
+					<div className="col-12 col-sm-12 col-md-4 col-lg-4 ">
+						<div class="card" aria-hidden="true">
+							<img src="..." class="card-img-top" alt="..." />
+							<div class="card-body">
+								<h5 class="card-title placeholder-glow">
+									<span class="placeholder col-6"></span>
+								</h5>
+								<p class="card-text placeholder-glow">
+									<span class="placeholder col-7"></span>
+									<span class="placeholder col-4"></span>
+									<span class="placeholder col-4"></span>
+									<span class="placeholder col-6"></span>
+									<span class="placeholder col-8"></span>
+								</p>
+								<a class="btn btn-primary disabled placeholder col-6"></a>
+							</div>
+						</div>
+					</div>
 					<div className="col-12 col-sm-12 col-md-4 col-lg-4 ">
 						<div class="card" aria-hidden="true">
 							<img src="..." class="card-img-top" alt="..." />
